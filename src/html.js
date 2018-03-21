6518eb12c2b71ccfea4e4b51a01e0c46ee1ffb24
Administrator <thaoamtanuyen@gmail.com>
2018-03-21 22:53:12 +0700

    up

--- src/html.js
+++ src/html.js
@@ -68,7 +68,7 @@ export default class HTML extends Component {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-1072469877861017',
+    google_ad_client: 'ca-pub-8152434656766908',
     enable_page_level_ads: true
   });
   `;
